7fa1efc645de5ce73c37b912d8dc40730a333c68
Ajay2196 <96917624+Ajay2196@users.noreply.github.com>
2021-12-31 15:01:51 +0000

    new User -  New commit
    committing from npm-install-aj

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var typed3 = new Typed('#typedAnim', {
+    strings: ['<strong> Web Developer</strong>', ' <strong> Software Consultant</strong>','<strong> Budding Interaction Designer</strong>',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay : 2000,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+
